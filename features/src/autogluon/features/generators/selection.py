66c109a9700ae1b6b1d5d5cf16c1f053710aca0a
bastis007 <schaefer.bastian@mail.de>
2026-01-21 14:36:56 +0100

    Add future feature selection filter methods

--- features/src/autogluon/features/generators/selection.py
+++ features/src/autogluon/features/generators/selection.py
@@ -5,29 +5,67 @@ from pandas import DataFrame, Series
 
 from .abstract import AbstractFeatureSelector
 
+
+from tabarena.benchmark.feature_selection_methods.ag.randomfs.RandomFS import RandomFS
+from tabarena.benchmark.feature_selection_methods.ag.original.Original import Original
+from tabarena.benchmark.feature_selection_methods.ag.enumeration.enumeration_fs import EnumerationFeatureSelector
+
+from tabarena.benchmark.feature_selection_methods.ag.t_test.tTest import tTest
+from tabarena.benchmark.feature_selection_methods.ag.rf_importance.RFImportance import RFImportance
+from tabarena.benchmark.feature_selection_methods.ag.information_gain.InformationGain import InformationGain
+from tabarena.benchmark.feature_selection_methods.ag.jmi.JMI import JMI
+from tabarena.benchmark.feature_selection_methods.ag.cmim.CMIM import CMIM
+from tabarena.benchmark.feature_selection_methods.ag.mrmr.mRMR import mRMR
+from tabarena.benchmark.feature_selection_methods.ag.gini.Gini import Gini
+from tabarena.benchmark.feature_selection_methods.ag.relieff.ReliefF import ReliefF
+from tabarena.benchmark.feature_selection_methods.ag.lasso.Lasso import Lasso
+from tabarena.benchmark.feature_selection_methods.ag.chi2.Chi2 import Chi2
+from tabarena.benchmark.feature_selection_methods.ag.laplaceian_score.LaplacianScore import LaplacianScore
+from tabarena.benchmark.feature_selection_methods.ag.fisher_score.FisherScore import FisherScore
+from tabarena.benchmark.feature_selection_methods.ag.disr.DISR import DISR
+
+from tabarena.benchmark.feature_selection_methods.ag.select_k_best_f.select_k_best_f import Select_k_Best_F
 from tabarena.benchmark.feature_selection_methods.ag.boruta.boruta import Boruta
+from tabarena.benchmark.feature_selection_methods.ag.mafese.MAFESE import MAFESE
+
 from tabarena.benchmark.feature_selection_methods.ag.ls_flip.ls_flip import LocalSearchFeatureSelector_Flip
 from tabarena.benchmark.feature_selection_methods.ag.ls_flipswap.ls_flipswap import LocalSearchFeatureSelector_FlipSwap
-from tabarena.benchmark.feature_selection_methods.ag.enumeration.enumeration_fs import EnumerationFeatureSelector
-from tabarena.benchmark.feature_selection_methods.ag.mafese.MAFESE import MAFESE
-from tabarena.benchmark.feature_selection_methods.ag.metafs.MetaFS import MetaFS
-from tabarena.benchmark.feature_selection_methods.ag.select_k_best_f.select_k_best_f import Select_k_Best_F
-from tabarena.benchmark.feature_selection_methods.ag.randomfs.RandomFS import RandomFS
 
+from tabarena.benchmark.feature_selection_methods.ag.metafs.MetaFS import MetaFS
 
 
 logger = logging.getLogger(__name__)
 
 
 FEATURE_SELECTION_METHODS = {
+    "Original": Original,
     "RandomFS": RandomFS,
+    "Enumeration": EnumerationFeatureSelector,
+
+    # Chosen Filter Methods
+    "tTest": tTest,
+    "RFImportance": RFImportance,
+    "InformationGain": InformationGain,
+    "JMI": JMI,
+    "CMIM": CMIM,
+    "mRMR": mRMR,
+    "Gini": Gini,
+    "ReliefF": ReliefF,
+    "Lasso": Lasso,
+    "Chi2": Chi2,
+    "LaplacianScore": LaplacianScore,
+    "FisherScore": FisherScore,
+    "DISR": DISR,
+
+    # Other methods
+    "SelectKBest": Select_k_Best_F,
     "Boruta": Boruta,
+    "Mafese": MAFESE,
+
     "LS_Flip": LocalSearchFeatureSelector_Flip,
     "LS_FlipSwap": LocalSearchFeatureSelector_FlipSwap,
-    "Enumeration": EnumerationFeatureSelector,
-    "Mafese": MAFESE,
+
     "MetaFS": MetaFS,
-    "SelectKBest": Select_k_Best_F,
 }
 
 
